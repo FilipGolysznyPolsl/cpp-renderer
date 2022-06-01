6ffbd35fae4f9a36e96583850ea79ba152f13352
BlackRabbitek <67788753+BlackRabbitek@users.noreply.github.com>
2022-06-01 13:52:59 +0200

    Tworzenie BMP

---
+++ cpp-renederer/cpp-renederer/obraz.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+struct Pixel {
+	float r, g, b;
+
+	Pixel();
+	Pixel(float r, float g, float b);
+	~Pixel();
+};
+class Obraz
+{
+public:
+	Obraz(int w, int h);
+	~Obraz();
+
+	Pixel GetKolor(int x, int y) const;
+	void SetKolor(const Pixel& kolor, int x, int y);
+
+	void Export(const char* path);
+
+private:
+	int height;
+	int width;
+	vector<Pixel> v_pixel;
+};
+
