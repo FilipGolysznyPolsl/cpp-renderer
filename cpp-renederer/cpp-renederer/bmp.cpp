c312d35999a127b3dd937e25550ba80843d136ae
BlackRabbitek <67788753+BlackRabbitek@users.noreply.github.com>
2022-06-01 13:54:32 +0200

    Add files via upload

---
+++ cpp-renederer/cpp-renederer/bmp.cpp
@@ -0,0 +1,18 @@
+﻿#include "obraz.h"
+
+int main()
+{
+	int w = 600;
+	int h = 600;
+
+	Obraz bmp(w, h);
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			if (x > y) {
+				bmp.SetKolor(Pixel(120, 120, 50), x, y);
+			}
+		}
+	}
+
+	bmp.Export("test.bmp");
+}
